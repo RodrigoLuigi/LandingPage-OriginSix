48daf298a9bad2682a2336efc12dc6ab9554e32d
RodrigoLuigi <digaor@gmail.com>
2022-03-16 20:58:38 -0300

    Ajustes e Responsividade
    foi adicionado media query para dispositivos de tela acima 1200px

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
 
-for(const element of toggle){
-  element.addEventListener('click', function(){
+for (const element of toggle) {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show');
   })
 }
@@ -12,23 +12,96 @@ for(const element of toggle){
 const links = document.querySelectorAll('nav ul li a');
 console.log(links)
 
-for(const link of links){
-  link.addEventListener('click', function(){
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show');
   })
 }
 
-/* mudar o header da página adicionando box-shadow quando der scroll*/
-
+/* Função adicionando box-shadow no header quando der scroll*/
 const header = document.querySelector('#header');
 const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY >= navHeight){
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
-  }else{
+  } else {
     header.classList.remove('scroll');
-
   }
-})
+}
 
+/* Testimonials carousel swiper (slider)*/
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+
+});
+
+/* Scroll Reveal , mostrar conteúdo ao rolar a pagina(scroll)*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1100,
+  reset: true
+})
+ScrollReveal().reveal(
+  `#home .image, #home .button, #home .text,
+  #about .image, #about .text ,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text,#contact .button, #contact .links,
+  footer .brand, footer .social
+   `, {
+    interval: 100
+  });
+
+/* Função voltar para o Topo*/
+const backToTopButton = document.querySelector('.back-to-top');
+
+function backToTop() {
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a seção visivel na página */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if(checkpointStart && checkpointEnd){
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+    }else{
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+    }
+  }
+}
+
+/* When scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+})
